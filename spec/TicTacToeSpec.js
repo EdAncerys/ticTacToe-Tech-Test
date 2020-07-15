b8a34570fa652cc73f06375d2b7134486814e56b
Ed <lookatemail@gmail.com>
2020-07-15 20:11:17 +0100

    Test Pass: player2 should have no fields claimed before start of the game

--- spec/TicTacToeSpec.js
+++ spec/TicTacToeSpec.js
@@ -8,4 +8,8 @@ describe('TicTacToe', () => {
   it('player1 should have no fields claimed before the start of the game', () => {
     expect(game.player1Fields().length).toEqual(0);
   });
+
+  it('player2 should have no fields claimed before the start of the game', () => {
+    expect(game.player2Fields().length).toEqual(0);
+  });
 });
--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,9 +1,14 @@
 class TicTacToe {
   constructor() {
     this.player1 = [];
+    this.player2 = [];
   }
 
   player1Fields() {
     return this.player1;
+  }
+
+  player2Fields() {
+    return this.player2;
   }
 }
