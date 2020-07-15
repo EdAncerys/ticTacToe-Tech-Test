ff429655a9f6b2c045f52f781e1dccdbccd2dbce
Ed <lookatemail@gmail.com>
2020-07-15 20:30:36 +0100

    Test pass: player1 able to claim a field and start first

--- spec/TicTacToeSpec.js
+++ spec/TicTacToeSpec.js
@@ -12,4 +12,9 @@ describe('TicTacToe', () => {
   it('player2 should have no fields claimed before the start of the game', () => {
     expect(game.player2Fields().length).toEqual(0);
   });
+
+  it('player1 should always start first and have field #1 claimed after the move', () => {
+    game.playGame(1);
+    expect(game.player1Fields().length).toEqual(1);
+  });
 });
--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -2,6 +2,7 @@ class TicTacToe {
   constructor() {
     this.player1 = [];
     this.player2 = [];
+    this.gameTurn = true;
   }
 
   player1Fields() {
@@ -10,5 +11,11 @@ class TicTacToe {
 
   player2Fields() {
     return this.player2;
+  }
+
+  playGame(field) {
+    if (this.gameTurn) this.player1.push(field);
+    // else this.player2.push(field);
+    // this.gameTurn = !this.gameTurn;
   }
 }
