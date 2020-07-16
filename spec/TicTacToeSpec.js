02c0ab01d867f54a28e80bf2f72c0e90115ea1df
Ed <lookatemail@gmail.com>
2020-07-16 12:53:56 +0100

    Test Pass: player2 wins if field 1,2,3 claimed

--- spec/TicTacToeSpec.js
+++ spec/TicTacToeSpec.js
@@ -62,5 +62,15 @@ describe('TicTacToe', () => {
       game.playGame(3);
       expect(game.gameWinner()).toEqual('Player1');
     });
+
+    it('player2 wins if field 3,4,5 claimed', () => {
+      game.playGame(4);
+      game.playGame(1);
+      game.playGame(5);
+      game.playGame(2);
+      game.playGame(9);
+      game.playGame(3);
+      expect(game.gameWinner()).toEqual('Player2');
+    });
   });
 });
--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -4,7 +4,11 @@ class TicTacToe {
     this.player1 = [];
     this.player2 = [];
     this.gameTurn = true;
-    this.winner = 'Player1';
+    this.winningFields = [
+      [1, 2, 3],
+      [0, 0, 0],
+    ];
+    this.winner;
   }
 
   player1Fields() {
@@ -22,8 +26,24 @@ class TicTacToe {
   playGame(field) {
     if (this.gameTurn && this.fields.includes(field)) this.player1.push(field);
     if (!this.gameTurn && this.fields.includes(field)) this.player2.push(field);
-
+    this._checkIfHaveWinner();
     this.fields = this.fields.filter((num) => num != field);
     this.gameTurn = !this.gameTurn;
+  }
+
+  _checkIfHaveWinner() {
+    let player;
+    if (this.gameTurn) player = this.player1;
+    else player = this.player2;
+
+    this.winningFields.forEach((combo) => {
+      if (combo.every((field) => player.includes(field))) {
+        this.winner = this._playerPicker();
+      }
+    });
+  }
+
+  _playerPicker() {
+    return this.gameTurn ? 'Player1' : 'Player2';
   }
 }
